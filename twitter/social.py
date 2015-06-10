9c85045d60ecbfb1751a5763b0525b43683b9887
Albert O'Connor <albert@albertoconnor.ca>
2015-06-10 14:15:16 -0400

    Add import

--- twitter/social.py
+++ twitter/social.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.timezone import now
 
 import tweepy
 
